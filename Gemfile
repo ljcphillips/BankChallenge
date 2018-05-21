893fb3be53d5e1c64d85cb25973ceddbf82ffc50
Lorraine Philllips <ljcphillips@gmail.com>
2018-05-21 11:45:29 +0100

    deposit money - second test

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@
 source "https://rubygems.org"
 
  gem "rspec"
+ gem 'rubocop-rspec'
+ gem 'simplecov', require: false, group: :test
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -1,9 +1,12 @@
 require 'bank_account.rb'
 describe BankAccount do
-    subject(:bank_account) { described_class.new}
+  subject(:bank_account) { described_class.new }
 
-      it "returns 0" do
-        expect(subject.balance).to eql(0)
-      end
+  it 'returns 0 when asked for bank.balance' do
+    expect(subject.balance).to eql(0)
+  end
 
-end      
+  it 'increase the balance by 100 when 100 is deposited'
+    subject.deposit(100)
+    expect(subject.balance). to eql(100)
+end
